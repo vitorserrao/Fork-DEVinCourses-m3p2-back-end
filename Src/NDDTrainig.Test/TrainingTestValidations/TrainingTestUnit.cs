fc7aa60e58a011342ad0a02c9d53b0903620db6c
Vitor Serrão <vitorwss@gmail.com>
2022-12-05 12:35:29 -0300

    Implementação de testes no Training, verificando se o Title é vazio ou não, e se RealaseDate está no passado em relação a data atual. Ou seja, data de lançamento tem que ser maior ou igual a data de publicação.

---
+++ Src/NDDTrainig.Test/TrainingTestValidations/TrainingTestUnit.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using FluentValidation.TestHelper;
+using NDDTraining.Domain.Validations;
+using NDDTraining.Domain.DTOS;
+using System;
+
+
+[TestFixture]
+public class TrainingTestUnit
+{
+
+    private TrainingValidation validator;
+    [SetUp]
+    public void Setup()
+    {
+        validator = new TrainingValidation();
+    }
+
+    [TestCase("Curso Angular")]
+    public void Validate_Title_in_TrainingDto_Sucess(string title)
+    {
+        var training = new TrainingDTO
+        {
+            Id = 1,
+            Url = "jijsddj",
+            Title = title,
+            Description = "jdnadasida",
+            Teacher = "skask",
+            Duration = new System.TimeSpan(),
+            Active = true,
+            Category = "tech",
+            ReleaseDate = new System.DateTime(1995, 10, 11)
+        };
+        var result = validator.TestValidate(training);
+        result.ShouldNotHaveValidationErrorFor(training => training.Title);
+
+    }
+    [TestCase("")]
+    public void Validate_Title_in_TrainingDto_Fail(string title)
+    {
+        var training = new TrainingDTO
+        {
+            Id = 1,
+            Url = "jijsddj",
+            Title = title,
+            Description = "jdnadasida",
+            Teacher = "skask",
+            Duration = new System.TimeSpan(),
+            Active = true,
+            Category = "tech",
+            ReleaseDate = new System.DateTime(1995, 10, 11)
+        };
+        var result = validator.TestValidate(training);
+        result.ShouldNotHaveValidationErrorFor(training => training.Title);
+
+    }
+
+    [TestCase("2023,10,10")]
+    public void Validate_RealaseDate_in_TrainingDto_Sucess(DateTime realaseDate)
+    {
+        var training = new TrainingDTO
+        {
+            Id = 1,
+            Url = "jijsddj",
+            Title = "curso x",
+            Description = "jdnadasida",
+            Teacher = "skask",
+            Duration = new System.TimeSpan(),
+            Active = true,
+            Category = "tech",
+            ReleaseDate = realaseDate
+        };
+        var result = validator.TestValidate(training);
+        result.ShouldNotHaveValidationErrorFor(training => training.ReleaseDate);
+
+    }
+
+    [TestCase("2021,10,10")]
+    public void Validate_RealaseDate_in_TrainingDto_Fail(DateTime realaseDate)
+    {
+        var training = new TrainingDTO
+        {
+            Id = 1,
+            Url = "jijsddj",
+            Title = "curso x",
+            Description = "jdnadasida",
+            Teacher = "skask",
+            Duration = new System.TimeSpan(),
+            Active = true,
+            Category = "tech",
+            ReleaseDate = realaseDate
+        };
+        var result = validator.TestValidate(training);
+        result.ShouldNotHaveValidationErrorFor(training => training.ReleaseDate);
+
+    }
+
+}
---
+++ Src/NDDTraining.Domain/Validations/TrainingValidation.cs
@@ -0,0 +1,26 @@
+﻿
+using FluentValidation;
+using NDDTraining.Domain.DTOS;
+
+namespace NDDTraining.Domain.Validations
+{
+    public class TrainingValidation : AbstractValidator<TrainingDTO>
+    {
+
+        public TrainingValidation()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .WithMessage("Campo não pode ser vazio");
+                
+
+            RuleFor(x => x.ReleaseDate)
+               .NotNull()
+               .GreaterThanOrEqualTo(DateTime.Now)
+               .WithMessage("Data de lançamento não pode ser no passado");
+
+
+
+        }
+    }
+}
